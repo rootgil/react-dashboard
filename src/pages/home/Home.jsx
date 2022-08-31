dc85431a0538ea096205b0ea561dd5208d4e6b58
rootgil <gillesahouantchede@gmail.com>
2022-08-31 04:02:03 +0200

    Homa page finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Table from '../../components/list/List'
 import Chart from '../../components/chart/Chart'
 import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
@@ -20,6 +21,10 @@ const Home = () => {
         <div className='charts'>
           <Featured/>
           <Chart/>
+        </div>
+        <div className='listContainer'>
+          <div className='listTitle'>Latest Transactions</div>
+          <Table/>
         </div>
       </div>
     </div>
